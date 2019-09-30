ed9516237f0039914ca458c204830814dc660150
Hunter Casbeer <hcasbeer@bayareanewsgroup.com>
2019-09-29 17:19:21 -0700

    added list ingredients feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,7 +1,7 @@
 
 from rest_framework import serializers
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -10,4 +10,13 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ('id', 'name')
-        read_only_Fields = ('id',)
+        read_only_fields = ('id',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredient objects"""
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -6,6 +6,7 @@ from recipe import views
 # default router registers all appropriate urls in our viewset
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
